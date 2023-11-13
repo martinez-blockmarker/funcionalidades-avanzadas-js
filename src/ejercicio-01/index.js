36cce3bf3b22ed5379eebd0f56ee77c1e1d90d55
Ainoha Armesto Martinez <lainos.aa@gmail.com>
2023-11-13 20:44:17 +0100

    Añadir a ejercicio 1

--- src/ejercicio-01/index.js
+++ src/ejercicio-01/index.js
@@ -1,1 +1,7 @@
 // Escribe tu código aquí:
+const numeros = [2, 4, 5, 6, 10]
+
+const numerosAlCuadrado = numero.map(numero=>numero + numero)
+
+console.log('Numeros al cuadrado', numerosAlCuadrado)
+
