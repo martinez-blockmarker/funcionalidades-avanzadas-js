8d025c44377ea9b3f238efc014137d0f1f2f0848
Ainoha Armesto Martinez <lainos.aa@gmail.com>
2023-11-14 14:34:00 +0100

    Añadir a estructura general

--- src/ejercicio-02/index.js
+++ src/ejercicio-02/index.js
@@ -1,1 +1,20 @@
 // Escribe tu código aquí:
+const person = {
+    name: 'Ainoha',
+    greet: function () {
+      console.log(`Hola, soy ${this.name}`)
+    }
+  }
+  
+  person.greet() 
+  
+  
+  const person2 = {
+    name: 'Doraemon',
+    greet: () => {
+      console.log(`Hola, soy ${this.name}`)
+    }
+  }
+  
+  person.greet()
+  
